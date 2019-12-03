dd4c22aad255a8b46256fa17f6b059a2eb0635be
andreamatt <andreamatte97@gmail.com>
2019-12-03 18:43:59 +0100

    added trentotoday format for date and time

--- scripts/extract/trentoTodayE_PRE.py
+++ scripts/extract/trentoTodayE_PRE.py
@@ -5,4 +5,11 @@ import requests
 def rm_main():
 	url = 'http://194.32.77.99/KDI/dataset/trentoTodayE.json'
 	obj = json.loads(requests.get(url).text)
+	
+	for event in obj['events']:
+		# if no category, get first tag
+		if 'category' not in event:
+			if 'tags' in event:
+				event['category'] = event['tags'][0]['name']
+				
 	return json.dumps(obj)
---
+++ scripts/format/trentoTodayE_format.py
@@ -0,0 +1,35 @@
+import json
+import re
+import requests
+
+def rm_main(JSONString):
+	obj = json.loads(JSONString)
+	
+	known_times = {
+		'tutto il giorno': '00:00-23:59'
+	}
+	
+	for event in obj['events']:
+		if 'date' in event:
+			dates = re.findall(r'\d\d/\d\d/\d\d\d\d', event['date'])
+			if len(set(dates)) == 1:
+				event['date'] = dates[0]
+			else:
+				event['date'] = f'{dates[0]}-{dates[1]}'
+		else:
+			event['date'] = ""
+
+		new_time = ""
+		if 'time' in event:
+			if event['time'] in known_times:
+				new_time = known_times[event['time']]
+			else:
+				r = r'(?:(?:\d\d|\d)[:.]\d\d)|(?:\d\d|\d)'
+				times = re.findall(r, event['time'])
+				if len(times)==1:
+					new_time = times[0]
+				elif len(times) == 2:
+					new_time = f'{times[0]}-{times[1]}'
+		event['time'] = new_time
+		
+	return json.dumps(obj)
