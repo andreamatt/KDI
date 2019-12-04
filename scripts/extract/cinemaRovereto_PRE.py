c6b6e39b7220a629a59672deb265f40ff5acdb92
andreaiossa <andrea.iossa@studenti.unitn.it>
2019-12-04 19:26:47 +0100

    cinemaRovereto PRE and FORMAT done

--- scripts/extract/cinemaRovereto_PRE.py
+++ scripts/extract/cinemaRovereto_PRE.py
@@ -4,21 +4,27 @@ import requests
 
 
 def rm_main():
-	url = 'https://raw.githubusercontent.com/andreamatt/KDI/master/dataset/cinemaRovereto.json'
-	obj = json.loads(requests.get(url).text)
-	obj['Location'] = "Piazza Rosmini 18/A. Rovereto (TN)"
-	obj['Contact'] = "0464 421216"
-	obj['Price'] = "8,50 €"
-	obj['URL'] = "http://www.supercinemarovereto.it/"
-	for movie in obj['movies']:
-		if 'time' not in movie:
-			obj['movies'].remove(movie)
-			continue
-		for dateAndTime in movie['time']:
-			if movie['title'] == "CHIUSO":
-				obj['movies'].remove(movie)
-				break
-			if isinstance(dateAndTime, str):
-				obj['movies'].remove(movie)
-				break
-	return json.dumps(obj)
+    url = 'https://raw.githubusercontent.com/andreamatt/KDI/master/dataset/cinemaRovereto.json'
+    obj = json.loads(requests.get(url).text)
+    info = {
+        'location': "Piazza Rosmini 18/A. Rovereto (TN)",
+        'contact': "0464 421216",
+        'price': "8,50 €",
+        'cinemaURL': "http://www.supercinemarovereto.it/"
+
+    }
+
+    for movie in obj['movies']:
+        if 'time' not in movie:
+            obj['movies'].remove(movie)
+            continue
+        for dateAndTime in movie['time']:
+            if movie['title'] == "CHIUSO":
+                obj['movies'].remove(movie)
+                break
+            if isinstance(dateAndTime, str):
+                obj['movies'].remove(movie)
+                break
+
+        movie.update(info)
+    return json.dumps(obj)
--- scripts/structure/cinemaRovereto.py
+++ scripts/structure/cinemaRovereto.py
@@ -2,71 +2,76 @@ import json
 import pandas as pd
 import requests
 
-#load classes
-classes_txt = requests.get('https://raw.githubusercontent.com/andreamatt/KDI/master/scripts/structure/classes.py').text
+# load classes
+classes_txt = requests.get(
+    'https://raw.githubusercontent.com/andreamatt/KDI/master/scripts/structure/classes.py').text
 
 
-#exec(classes_txt)
+# exec(classes_txt)
 def GeneralEvent(name, price, description, website, duration, language, isTicketAvailable):
-	return locals()
+    return locals()
 
 
 def Time(startDate, endDate, startTime, endTime):
-	return locals()
+    return locals()
 
 
 def Period(byDay, byMonth, byMonthDay, repeatCount, repeatFrequency):
-	return locals()
+    return locals()
 
 
 def Discount(young, students, children, groups):
-	return locals()
+    return locals()
 
 
 def CreativeWork(name, creator, createDate, url):
-	return locals()
+    return locals()
 
 
 def ScienceEvent(topic):
-	return locals()
+    return locals()
 
 
 def VisualArtsEvent(artMovement, style):
-	return locals()
+    return locals()
 
 
 def MusicEvent(genre, performer, songs):
-	return locals()
+    return locals()
 
 
 def ScreeningEvent(salaNumber, originalName, genre, duration):
-	return locals()
+    return locals()
 
 
 def TheatreEvent(interpreters, genre):
-	return locals()
+    return locals()
 
 
 def TalkEvent(hasPresentation, isConference, pages, genre, editor):
-	return locals()
+    return locals()
 
 
 def rm_main(JSONString):
-	with open('C:/Users/andre/Desktop/kdi/scraping/KDI/temp.json', 'w') as outfile:
-		json.dump(json.loads(JSONString), outfile, indent="\t")
+    with open('C:/Users/andre/Desktop/kdi/scraping/KDI/temp.json', 'w') as outfile:
+        json.dump(json.loads(JSONString), outfile, indent="\t")
 
-	events = []
-	cinemaRovereto = json.loads(JSONString)
-	for movie in cinemaRovereto['movies']:
-		gen = GeneralEvent(movie['title'], movie['Price'], movie['description'], movie['cinemaURL'], movie['length'], movie['language'], True)
-		screen = ScreeningEvent('', movie['originalName'], movie['genre'], movie['length'])
-		work = CreativeWork(movie['title'], movie['director'], movie['createDate'], movie['URL'])
+    events = []
+    cinemaRovereto = json.loads(JSONString)
+    for movie in cinemaRovereto['movies']:
+        gen = GeneralEvent(movie['title'], movie['Price'], movie['description'],
+                           movie['cinemaURL'], movie['length'], movie['language'], True)
+        screen = ScreeningEvent(
+            '', movie['originalName'], movie['genre'], movie['length'])
+        work = CreativeWork(
+            movie['title'], movie['director'], movie['createDate'], movie['URL'])
 
-		for dateAndTime in movie['time']:
-			date = Time(date['day'], date['day'], date['startTime'], date['endTime'])
-			event = {**gen, **screen, **work, **date}
-			events.append(event)
+        for dateAndTime in movie['time']:
+            date = Time(date['day'], date['day'],
+                        date['startTime'], '')
+            event = {**gen, **screen, **work, **date}
+            events.append(event)
 
-	events = [ob.__dict__ for ob in events]
-	df = pd.DataFrame(events)
-	return df
+    events = [ob.__dict__ for ob in events]
+    df = pd.DataFrame(events)
+    return df
