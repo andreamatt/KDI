3c8a14d746fa508aa37bd6c89cf19fabfc13b881
andreamatt <andreamatte97@gmail.com>
2019-12-04 18:51:59 +0100

    new structure

--- scripts/structure/cinemaRovereto.py
+++ scripts/structure/cinemaRovereto.py
@@ -1,72 +1,72 @@
 import json
-
 import pandas as pd
 import requests
 
+#load classes
+classes_txt = requests.get('https://raw.githubusercontent.com/andreamatt/KDI/master/scripts/structure/classes.py').text
 
-class eventObj:
 
-	def __init__(self,
-	             title="",
-	             ScienceEvent=False,
-	             VisualArtsEvent=False,
-	             MusicEvent=False,
-	             ScreeningEvent=False,
-	             TheatreEvent=False,
-	             TalkEvent=False,
-	             GeneralEvent=False,
-	             date="",
-	             time="",
-	             locationName="",
-	             locationURL="",
-	             suitableFor="",
-	             source="",
-	             description="",
-	             other="",
-	             contact="",
-	             cost="",
-	             link=""):
-		self.source = source.replace("\n", " ,")
-		self.ScienceEvent = ScienceEvent
-		self.VisualArtsEvent = VisualArtsEvent
-		self.MusicEvent = MusicEvent
-		self.ScreeningEvent = ScreeningEvent
-		self.TheatreEvent = TheatreEvent
-		self.TalkEvent = TalkEvent
-		self.GeneralEvent = GeneralEvent
-		self.suitableFor = suitableFor.replace("\n", " ,")
-		self.title = title.replace("\n", " ,")
-		self.date = date.replace("\n", " ,")
-		self.time = time.replace("\n", " ,")
-		self.locationName = locationName.replace("\n", " ,")
-		self.locationURL = locationURL.replace("\n", " ,")
-		self.description = description.replace("\n", " ,")
-		self.contact = contact.replace("\n", " ,")
-		self.cost = cost.replace("\n", " ,")
-		self.other = other.replace("\n", " ,")
-		self.link = link.replace("\n", " ,")
+#exec(classes_txt)
+def GeneralEvent(name, price, description, website, duration, language, isTicketAvailable):
+	return locals()
+
+
+def Time(startDate, endDate, startTime, endTime):
+	return locals()
+
+
+def Period(byDay, byMonth, byMonthDay, repeatCount, repeatFrequency):
+	return locals()
+
+
+def Discount(young, students, children, groups):
+	return locals()
+
+
+def CreativeWork(name, creator, createDate, url):
+	return locals()
+
+
+def ScienceEvent(topic):
+	return locals()
+
+
+def VisualArtsEvent(artMovement, style):
+	return locals()
+
+
+def MusicEvent(genre, performer, songs):
+	return locals()
+
+
+def ScreeningEvent(salaNumber, originalName, genre, duration):
+	return locals()
+
+
+def TheatreEvent(interpreters, genre):
+	return locals()
+
+
+def TalkEvent(hasPresentation, isConference, pages, genre, editor):
+	return locals()
 
 
 def rm_main(JSONString):
+	with open('C:/Users/andre/Desktop/kdi/scraping/KDI/temp.json', 'w') as outfile:
+		json.dump(json.loads(JSONString), outfile, indent="\t")
+
 	events = []
 	cinemaRovereto = json.loads(JSONString)
 	for movie in cinemaRovereto['movies']:
+		gen = GeneralEvent(movie['title'], movie['Price'], movie['description'], movie['cinemaURL'], movie['length'], movie['language'], True)
+		screen = ScreeningEvent('', movie['originalName'], movie['genre'], movie['length'])
+		work = CreativeWork(movie['title'], movie['director'], movie['createDate'], movie['URL'])
+
 		for dateAndTime in movie['time']:
-			events.append(
-			    eventObj(
-			        source="cinemaRovereto",
-			        title=movie['title'],
-			        description=movie['description'],
-			        VisualArtsEvent=True,
-			        date=dateAndTime['day'],
-			        time=dateAndTime['hour'],
-			        contact=cinemaRovereto['Contact'],
-			        cost=cinemaRovereto['Price'],
-			        locationName=cinemaRovereto['Location'],
-			        locationURL=cinemaRovereto['URL'],
-			        link=movie['Link']
-			        # length :-??
-			    ))
+			date = Time(date['day'], date['day'], date['startTime'], date['endTime'])
+			event = {**gen, **screen, **work, **date}
+			events.append(event)
+
 	events = [ob.__dict__ for ob in events]
 	df = pd.DataFrame(events)
 	return df
