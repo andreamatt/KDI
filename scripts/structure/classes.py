f324830a22b5fc84fee8022dc6d75618251fe5f3
andreamatt <andreamatte97@gmail.com>
2019-12-04 17:59:53 +0100

    modified base classes

---
+++ scripts/structure/classes.py
@@ -0,0 +1,42 @@
+def GeneralEvent(name, price, description, website, duration, language, isTicketAvailable):
+	return locals()
+
+
+def Time(startDate, endDate, startTime, endTime):
+	return locals()
+
+
+def Period(byDay, byMonth, byMonthDay, repeatCount, repeatFrequency):
+	return locals()
+
+
+def Discount(young, students, children, groups):
+	return locals()
+
+
+def CreativeWork(name, creator, createDate, url):
+	return locals()
+
+
+def ScienceEvent(topic):
+	return locals()
+
+
+def VisualArtsEvent(artMovement, style):
+	return locals()
+
+
+def MusicEvent(genre, performer, songs):
+	return locals()
+
+
+def ScreeningEvent(salaNumber, originalName, genre, duration):
+	return locals()
+
+
+def TheatreEvent(interpreters, genre):
+	return locals()
+
+
+def TalkEvent(hasPresentation, isConference, pages, genre, editor):
+	return locals()
